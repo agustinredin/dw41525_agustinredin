3bd17741ad61c2609dc4814ee32f616b904ca004
nombre1 <agustin.redin@vigiloo.com.ar>
2022-10-13 00:23:36 -0300

    Entrega del proyecto final

---
+++ readme.md
@@ -0,0 +1,8 @@
+Este sitio web es un proyecto real llevado a cabo para mi amigo Ignacio. Como se puede ver, él es personal trainer.
+
+Si se ve el código en detenimiento, lo que quiero hacer es estandarizar el html usando bootstrap asi puedo reutilizarlo para seguir con mis proyectos personales. Obviamente, complementando un poco con SASS.
+
+El scss está dividido en secciones, cada una correspondiente a un componente. Podían tener cada una su partial e importar todo pero eran varios archivos que, para mantener la simpleza, dejé en uno solo. 
+
+Muchisimas gracias de antemano por tomarse el tiempo de corregir y de ver mi proyecto en detalle. Me gustaría poder estar en el showcase de proyectos de alumnos dentro de la pagina de coder, si es posible y doy la talla. Saludos!
+
--- scripts/app.js
+++ scripts/app.js
@@ -3,14 +3,8 @@ const init = () => {
     window.onload = function () {
         window.scrollTo(0, 0);
 
-        console.log("29.9: pre generics v2 - incluido SEO y SASS a fondo")
+        console.log("proj final entregado")
       }
-
-    //   $(document).on("scroll", function(ev){
-    //     if(window.scrollY > 400) {
-    //         $(".features").addClass("active")
-    //     }
-    //   })
 
     $("form").on("submit", (e) => {
         e.preventDefault();
@@ -21,10 +15,10 @@ const init = () => {
     jQuery(function() {
         setTimeout(__scroll, 800)
     })
-    
+
     const setService = (service) => {
         let select = $("select[name=service]");
-    
+
         select.val(service)
     
         select.toggleClass("focus-visible")
@@ -33,17 +27,22 @@ const init = () => {
     const __scroll = () => {
         $("body").css("overflow-y", "scroll");
         console.log("scroll!")
-        
-        // SmoothScroll(document, 120, 12)
     }
-
-    $("#hamburger").on("click", displayMenu)
-
-    $("#hamburger").on("touchstart", displayMenu)
+    
+    $("#hamburger").on("click", function(e) {
+        if($("#navbarTogglerIcon").hasClass("fa-bars")){
+            $("#navbarTogglerIcon").removeClass("fa-bars")
+            $("#navbarTogglerIcon").addClass("fa-times")
+            return;
+        }
+        $("#navbarTogglerIcon").addClass("fa-bars")
+        $("#navbarTogglerIcon").removeClass("fa-times")
+    })
 
     let main = $("main").attr("class");
 
-    if(main.split('__')[1] == 'contact') {
+    console.log(main)
+    if(main.includes("contact")) {
         let serviceSelected = window.location.href.split('?q=');
         if(serviceSelected.length > 1)
         {
@@ -51,88 +50,6 @@ const init = () => {
         }
     }
 
-}
-
-// const SmoothScroll = (target, speed, smooth) => {
-//     if (target === document)
-//         target = (document.scrollingElement
-//             || document.documentElement
-//             || document.body.parentNode
-//             || document.body) // cross browser support for document scrolling
-//     var moving = false
-//     var pos = target.scrollTop
-//     var frame = target === document.body
-//         && document.documentElement
-//         ? document.documentElement
-//         : target // safari is the new IE
-
-//     target.addEventListener('mousewheel', scrolled, { passive: false })
-//     target.addEventListener('DOMMouseScroll', scrolled, { passive: false })
-
-//     function scrolled(e) {
-//         e.preventDefault(); // disable default scrolling
-
-//         var delta = normalizeWheelDelta(e)
-
-//         pos += -delta * speed
-//         pos = Math.max(0, Math.min(pos, target.scrollHeight - frame.clientHeight)) // limit scrolling
-
-//         if (!moving) update()
-//     }
-
-//     function normalizeWheelDelta(e) {
-//         if (e.detail) {
-//             if (e.wheelDelta)
-//                 return e.wheelDelta / e.detail / 40 * (e.detail > 0 ? 1 : -1) // Opera
-//             else
-//                 return -e.detail / 3 // Firefox
-//         } else
-//             return e.wheelDelta / 120 // IE,Safari,Chrome
-//     }
-
-//     function update() {
-//         moving = true
-
-//         var delta = (pos - target.scrollTop) / smooth
-
-//         target.scrollTop += delta
-
-//         if (Math.abs(delta) > 0.5)
-//             requestFrame(update)
-//         else
-//             moving = false
-//     }
-
-//     var requestFrame = function () { // requestAnimationFrame cross browser
-//         return (
-//             window.requestAnimationFrame ||
-//             window.webkitRequestAnimationFrame ||
-//             window.mozRequestAnimationFrame ||
-//             window.oRequestAnimationFrame ||
-//             window.msRequestAnimationFrame ||
-//             function (func) {
-//                 window.setTimeout(func, 1000 / 50);
-//             }
-//         );
-//     }()
-// }
-
-let openMenu = false;
-
-const displayMenu = () => {
-    console.log("f() displayMenu")
-    
-    let elemArray = $("nav a");
-    let prop = !openMenu ? "block" : "none";
-
-    for (let i = 0; i < elemArray.length; i++) {
-        elemArray[i].style.display = prop;
-    }
-
-    $(".header__container").toggleClass("header__container-open");
-    $(".header__logo").toggleClass("header__logo-hidden");
-
-    openMenu = prop == "block";
 }
 
 init()
